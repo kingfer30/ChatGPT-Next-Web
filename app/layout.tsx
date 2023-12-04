df3871196106c0e881c0e9218a057dc3801ff52c
KingFer <kingfer30@qq.com>
2023-12-04 23:54:41 +0800

    备注更新, 统计更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,11 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AI果果科技站",
+  title: "AI果果 | Next Web",
   description:
-    "OpenAI 接口聚合管理，支持多种渠道包括 Azure, PaLM2, Claude, 阿里通义, 文心一言, 讯飞星火, 智谱 ChatGLM, 360 智脑, OpenAI-SB, API2D, OhMyGPT, AI Proxy, CloseAI等众多平台",
+    "ChatGPT Next Web二开的在线聊天, 智能画图识图,基于Reac,新增最新模型gpt-3.5-turbo-1106,gpt-4-1106-preview,dall-e-3,tts,gpt-4-vision-preview等",
   keywords:
-    "AI果果, 果果API, ChatGPT, OpenAi接口, 中转, GPT中转分发, API Key中转分发, GPT3.5中转分发, GPT4.0中转分发, 分发平台, 中转平台",
+    "ChatGPT Next Web, React, 二开, 在线聊天, 画图, 绘图, 识图, Vision识图, 文心一言, One API, 中转",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "AI果果科技站",
+    title: "AI果果 | Next Web",
     statusBarStyle: "default",
   },
 };
